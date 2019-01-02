abf85f00b470ffb4fbc02ebf2d9974a4599113ae
Tony Garcia <tonysk8@gmx.net>
2019-01-02 11:27:40 -0600

    Blackened code.

--- beat/beat.py
+++ beat/beat.py
@@ -1,44 +1,50 @@
-'''Module to calculate beats(Swatch Internet Time).
-'''
+"""Module to calculate beats(Swatch Internet Time).
+"""
 
 import argparse
 import pkg_resources
 from datetime import datetime
 
+
 def internettime(hours, minutes, seconds, tzone):
-    '''Returns time(Swatch Internet Time) in beats.
-    '''
-    itime = (((seconds + (minutes * 60) + ((hours + tzone + 1) * 3600)) / 86.4)
-            % 1000)
+    """Returns time(Swatch Internet Time) in beats.
+    """
+    itime = ((seconds + (minutes * 60) + ((hours + tzone + 1) * 3600)) / 86.4) % 1000
     beats = int(itime)
     centibeats = str(itime).split(".")[1][0:3]
-    return '@{0}.{1}'.format(beats, centibeats)
+    return "@{0}.{1}".format(beats, centibeats)
+
 
 def now():
-    '''Gets the current time in beats.
-    '''
+    """Gets the current time in beats.
+    """
     utc_now = datetime.utcnow()
     beats = internettime(utc_now.hour, utc_now.minute, utc_now.second, tzone=0)
     return beats
 
+
 def main():
-    '''The main function.
-    '''
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('-t', '--time', help='Time to convert (HH:MM:SS)')
-    parser.add_argument('-z', '--timezone', metavar='TZ', type=float,
-                        help='Timezone in hours, default: UTC+01',
-                        default=1)
-    parser.add_argument('-v', '--version', action='store_true')
+    """The main function.
+    """
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("-t", "--time", help="Time to convert (HH:MM:SS)")
+    parser.add_argument(
+        "-z",
+        "--timezone",
+        metavar="TZ",
+        type=float,
+        help="Timezone in hours, default: UTC+01",
+        default=1,
+    )
+    parser.add_argument("-v", "--version", action="store_true")
     args = parser.parse_args()
 
     if args.version:
-        print('.beat v{0}'.format(pkg_resources.get_distribution("beat").version))
+        print(".beat v{0}".format(pkg_resources.get_distribution("beat").version))
         return
     if args.time:
         hours, minutes, seconds = args.time.split(":")
-        print(internettime(int(hours), int(minutes), int(seconds),
-                           args.timezone))
+        print(internettime(int(hours), int(minutes), int(seconds), args.timezone))
     else:
         beats = now()
         print(beats)
--- setup.py
+++ setup.py
@@ -3,19 +3,15 @@
 import setuptools
 
 setuptools.setup(
-    name='beat',
-    version='0.2.0',
-    description=('Swatch Internet Time conversion(beats)'),
-    author='Tony Garcia',
-    author_email='tonysk8@gmx.net',
-    entry_points={
-        'console_scripts': [
-            'beat=beat.beat:main'
-        ]
-    },
-    packages=['beat'],
-    url = 'https://github.com/tonyskapunk/beat',
-    download_url = 'https://github.com/tonyskapunk/beat/tarball/0.2.0',
-    license = "GNU General Public License v3 (GPLv3)",
-    zip_safe=False
+    name="beat",
+    version="0.2.0",
+    description=("Swatch Internet Time conversion(beats)"),
+    author="Tony Garcia",
+    author_email="tonysk8@gmx.net",
+    entry_points={"console_scripts": ["beat=beat.beat:main"]},
+    packages=["beat"],
+    url="https://github.com/tonyskapunk/beat",
+    download_url="https://github.com/tonyskapunk/beat/tarball/0.2.0",
+    license="GNU General Public License v3 (GPLv3)",
+    zip_safe=False,
 )
